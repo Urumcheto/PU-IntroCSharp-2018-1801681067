1e91a38b84e250141240c1e9b7882f363583d0c1
Mitaka <Mitaka@DESKTOP-7G8DDEU>
2018-11-11 18:08:21 +0200

    HomeWork Week 6

---
+++ STDB2 PUIntroHomeworks - 1801681067/StudentnamesAsNumberSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StudentnamesAsNumberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] names = System.IO.File.ReadAllLines(@"D:\PU-IntroCSharp-2018-1801681067\STDB2 PUIntroHomeworks - 1801681067\week6H6\StudentNames.txt");
+            int nameValue = 0;
+            char smileyFace = '\u263a';
+            foreach (string name in names)
+            {
+                foreach (char ch in name)
+                {
+                    nameValue = nameValue + ch;
+
+                }
+                if (nameValue > 1500) Console.WriteLine("{0} student's sum of names is {1} {2}", name, nameValue, smileyFace);
+                else Console.WriteLine("{0} student's sum of names is {1}", name, nameValue);
+                nameValue = 0;
+
+
+            }
+        }
+    }
+}
